a4114cc350df9085fb26758f2c1dd0a668515920
rihem <rihemebenhassan@insat.u-carthage.tn>
2021-04-11 11:46:09 +0100

    starting the project

---
+++ persocoach/src/main/java/com/website/persocoach/PersocoachApplication.java
@@ -0,0 +1,13 @@
+package com.website.persocoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersocoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersocoachApplication.class, args);
+	}
+
+}
---
+++ persocoach/src/test/java/com/website/persocoach/PersocoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.persocoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersocoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
