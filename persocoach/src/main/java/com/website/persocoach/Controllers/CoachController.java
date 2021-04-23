eb7e27b1c7521a606fead32bc8c0e654393c9627
rihem <rihemebenhassan@insat.u-carthage.tn>
2021-04-23 02:40:06 +0100

    mongo config + testing API

--- persocoach/src/main/java/com/website/persocoach/Controllers/CoachController.java
+++ persocoach/src/main/java/com/website/persocoach/Controllers/CoachController.java
@@ -3,6 +3,7 @@ package com.website.persocoach.Controllers;
 import com.website.persocoach.Models.Coach;
 import com.website.persocoach.repositories.CoachRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +12,7 @@ import java.net.URISyntaxException;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/catalog")
 public class CoachController {
 
     @Autowired
@@ -19,35 +20,40 @@ public class CoachController {
 
     CoachController (CoachRepository repo){
         super();
+
         this.repository=repo;
+        repository.deleteById("0");
+
     }
     @RequestMapping(value="/coaches",method = RequestMethod.GET)
     public Collection<Coach> coaches() {
         return repository.findAll();
     }
-    @RequestMapping(value="/coach/{id}",method= RequestMethod.GET)
-    public Coach getCoach(@PathVariable int id){
-        return repository.findById(Integer.toString(id)).orElse(null);
+
+
+   @RequestMapping(value="/coach/{id}",method= RequestMethod.GET)
+    public Coach getCoach(@PathVariable String id){
+        return repository.findById(id).orElse(null);
    }
 
-   @RequestMapping(value="/coach/delete/{id}", method=RequestMethod.DELETE)
-   public ResponseEntity<?> deleteCoach(@PathVariable int id){
-      repository.deleteById(Integer.toString(id));
+  @RequestMapping(value="/coach/delete/{id}", method=RequestMethod.DELETE)
+   public ResponseEntity<?> deleteCoach(@PathVariable String id){
+      repository.deleteById(id);
        return ResponseEntity.ok().build();
    }
 
-    @RequestMapping(value="/coach/update/{id}", method=RequestMethod.PUT)
-    public ResponseEntity<Coach>  updateCoach(@RequestBody Coach c){
-
+   @RequestMapping(value="/coach/update/{id}", method=RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Coach> updateCoach(@RequestBody Coach c){
        Coach co=  repository.save(c);
         return ResponseEntity.ok().body(co);
     }
 
 
     @RequestMapping(value="/coach/add", method=RequestMethod.PUT)
-    public ResponseEntity<Coach>  addCoach( Coach c) throws URISyntaxException {
+    public ResponseEntity<Coach>  addCoach(Coach c) throws URISyntaxException {
+
         Coach co=  repository.save(c);
-        return ResponseEntity.created(new URI("/coach/add" + co.getId())).body(co);
+        return ResponseEntity.created(new URI("/coach/add" +co.getId())).body(co);
 
     }
 
--- persocoach/src/main/java/com/website/persocoach/PersocoachApplication.java
+++ persocoach/src/main/java/com/website/persocoach/PersocoachApplication.java
@@ -1,10 +1,16 @@
 package com.website.persocoach;
 
+import com.website.persocoach.Models.Coach;
+import com.website.persocoach.repositories.CoachRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PersocoachApplication {
+	@Autowired
+	CoachRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersocoachApplication.class, args);
