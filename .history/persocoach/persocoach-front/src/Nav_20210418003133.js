de58d244d3c2dac218c2b321071b795a7c449296
rihemebh <rihemebenhassan@insat.u-carthage.tn>
2021-04-30 02:34:23 +0100

    delete useless folder

--- .history/persocoach/persocoach-front/src/Nav_20210418003133.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {NavBar, Nav, NavItem} from 'react-bootstrap';
-export default function Nav() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
