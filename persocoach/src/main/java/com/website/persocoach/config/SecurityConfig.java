5805ae0d725eebb8e326643574f29b76c9ad8e69
CyrineZaouali <cyrine.zaouali@insat.u-carthage.tn>
2021-05-16 17:37:17 +0100

    security config

---
+++ persocoach/src/main/java/com/website/persocoach/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.website.persocoach.config;
+
+import com.website.persocoach.security.jwt.JwtFilterRequest;
+import com.website.persocoach.security.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired private UserDetailsServiceImpl userDetailsService;
+    @Autowired private JwtFilterRequest jwtFilterRequest;
+
+
+    @Bean
+    public JwtFilterRequest authenticationJwtTokenFilter(){
+        return new JwtFilterRequest();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder()   ;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests().antMatchers("/api/auth/**", "/Coaches/catalog").permitAll()
+                .antMatchers("/api/test/**").permitAll()
+                .anyRequest().authenticated()
+        ;
+
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
